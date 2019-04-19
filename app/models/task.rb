12794337e35afd44a72d4ffbea904745f55caca7
Landon Epps <landon.epps@gmail.com>
2019-04-19 16:45:21 +0900

    :none is a reserved word

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,5 +3,5 @@ class Task < ApplicationRecord
   has_and_belongs_to_many :tags
 
   enum status: [:not_started, :started, :complete]
-  enum priority: [:none, :low, :medium, :high]
+  enum priority: [:low, :medium, :high]
 end
