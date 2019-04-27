795363587f3d475dff81ddcb101600c33fb997ae
Landon Epps <landon.epps@gmail.com>
2019-04-27 21:11:09 +0900

    Built out task list and task detail pages.

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,13 @@
 module TasksHelper
+  ##
+  # Returns the Font Awesome icon name for the task's status
+  def status_icon(task)
+    if task.started?
+      "dot-circle"
+    elsif task.complete?
+      "check-circle"
+    else
+      "circle"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'tasks#index'
 
-  resources :tasks
+  resources :tasks, except: :index
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
