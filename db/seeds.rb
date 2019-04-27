f2f8dee3b12f4e4ae1634b1efd0fe64bf14b8f9e
Landon Epps <landon.epps@gmail.com>
2019-04-28 01:48:36 +0900

    Have the db pouplate with seeds.rb for testing/development.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# TODO: Remove seed data. This is only for development.
+
+user = User.create(username: "user1", password: "foobar",
+                   password_confirmation: "foobar")
+
+tag1 = user.tags.create(name: "tag1")
+tag2 = user.tags.create(name: "tag2")
+
+sample_task = Task.new(user_id: 1, name: "A sample task in progress",
+                       deadline: 10.days.from_now,
+                       priority: :low, status: :started)
+sample_task.tags << tag1
+sample_task.save
+
+sample_task = Task.new(user_id: 1, name: "A completed sample task",
+                       deadline: 14.days.from_now,
+                       priority: :low, status: :complete)
+sample_task.tags << tag1
+sample_task.tags << tag2
+sample_task.save
+
+sample_task = Task.new(user_id: 1,
+                       name: "A sample task with a moderately long name",
+                       deadline: 20.days.from_now,
+                       priority: :low, status: :not_started)
+sample_task.tags << tag2
+sample_task.save
