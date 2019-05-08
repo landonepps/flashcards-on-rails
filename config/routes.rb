a811add9375d52aca336a1dca37b27de81642071
Landon Epps <landon.epps@gmail.com>
2019-05-08 16:45:13 +0900

    Made '/' the default path for tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'tasks#index'
 
-  resources :tasks, except: :index
+  resources :tasks, except: :index, path: '/'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
