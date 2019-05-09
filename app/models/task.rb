19f582472b8ad79ab80b9808f3f5de09ec8bf7a2
Landon Epps <landon.epps@gmail.com>
2019-05-09 15:33:08 +0900

    Gave status a default value so the status for new tasks is not_started instead of nil

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,9 @@ class Task < ApplicationRecord
   belongs_to :user
   has_and_belongs_to_many :tags
 
+  # set the default value for status to not_started
+  attribute :status, :integer, default: :not_started
+
   enum status: [:not_started, :started, :complete]
   enum priority: [:low, :medium, :high]
 end
